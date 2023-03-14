3eef8e213fa1a6f7f1d6ace74bb773c29849fbc6
dmai <karlheyerv@gmail.com>
2023-03-13 18:11:22 -0700

    assembly reset

--- chem_templates/imports.py
+++ chem_templates/imports.py
@@ -1,7 +1,7 @@
 import os
 from typing import Union, Optional, Callable, Tuple, Any
 from collections import OrderedDict, defaultdict
-from itertools import permutations
+from itertools import permutations, product, chain, islice
 import re
 import random
 import warnings
@@ -9,6 +9,7 @@ import time
 import pickle
 import json
 from multiprocessing import Pool
+from functools import partial
 
 # external
 import numpy as np
